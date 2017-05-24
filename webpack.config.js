b353ad17adf317c7a2748351df91af373bf2ca9f
Landon <landon.anderton@gmail.com>
2017-05-24 16:52:03 -0600

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,68 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'source-map',
   output: {
     path: __dirname + '/dist/',
     filename: 'index.js',
     libraryTarget: 'umd'
   },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
+    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+  ],
   module: {
-    loaders: [
+    rules: [
       {
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
         test: /\.js$/,
-        loader: 'babel-loader'
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+            plugins: [
+              'transform-object-rest-spread',
+              'babel-plugin-add-module-exports',
+              'babel-plugin-transform-class-properties'
+            ]
+          },
+        }],
       }
-    ]
+    ],
   },
-  resolve: {
-    // Can require('file') instead of require('file.js') etc.
-    extensions: ['', '.js', '.json']
-  },
-  externals: {
-    react: 'react'
-  },
-  plugins: []
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+    {
+      'react-bootstrap': {
+        root: 'ReactBootstrap',
+        commonjs2: 'react-bootstrap',
+        commonjs: 'react-bootstrap',
+        amd: 'react-bootstrap',
+      },
+    },
+    {
+      'classnames': {
+        root: 'classNames',
+        commonjs2: 'classnames',
+        commonjs: 'classnames',
+        amd: 'classnames',
+      },
+    },
+  ],
 };
